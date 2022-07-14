0b439e8b832f1d34ca20ca13935711f4cf13a352
Yassine Sebbar <yassine.sebbar@hotmail.com>
2022-07-15 00:04:42 +0200

    Finished profiles for automapper

---
+++ Profiles/ProductTypesProfile.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using coolblue_assesment.Dtos;
+using coolblue_assesment.Models;
+
+namespace coolblue_assesment.Profiles
+{
+    public class ProductTypesProfile : Profile
+    {
+        public ProductTypesProfile()
+        {
+            CreateMap<ProductType, ProductTypeReadDto>();
+        }
+    }
+}
